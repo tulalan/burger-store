c41df7853aa02ed54f1b40655cff641956f414e5
Nguyễn Tuấn Sơn <60498812+tulalan@users.noreply.github.com>
2020-08-27 17:47:36 +0800

    Update Burger.js
    nguyễn tuấn sơn edit

--- src/component/Burger.js
+++ src/component/Burger.js
@@ -3,7 +3,22 @@ import {connect} from 'react-redux'
 import '../component/BurgerCss.css';
 
 class Burger extends Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            statusUpdate: fasle,
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.burger !== prevProps.burger) {
+            this.setState({
+                statusUpdate: !this.statusUpdate
+            })
+        }
+    }
+    // trên đây là cách để hàm render chạy lại khi có props thay đổi
+    
+    
     renderBurger = ()=>{
         return this.props.burger.map((item,index)=>{
             return <div key={index}>
@@ -12,7 +27,6 @@ class Burger extends Component {
         })
     }
     render() {
-
         console.log(this.renderBurger())
         return (
             <div>
